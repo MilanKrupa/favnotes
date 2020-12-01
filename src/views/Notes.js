136a1835df8a30fc7e9e63d155cee44ad7f2e141
Milan_Krupa <milan.krupa9521@gmail.com>
2020-12-01 19:38:12 +0100

    Add notes fetching

--- src/views/Notes.js
+++ src/views/Notes.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import GridTemplate from 'templates/GridTemplate';
 import Card from 'components/molecules/Card/Card';
+import { fetchItems } from 'actions';
 
-const Notes = ({ notes }) => (
-  <GridTemplate pageType="notes">
-    {notes.map(({ title, content, _id }) => (
-      <Card title={title} content={content} _id={_id} key={_id} />
-    ))}
-  </GridTemplate>
-);
+class Notes extends Component {
+  componentDidMount() {
+    const { fetchNotes } = this.props;
+    fetchNotes();
+  }
+
+  render() {
+    const { notes } = this.props;
+
+    return (
+      <GridTemplate>
+        {notes.map(({ title, content, _id: id }) => (
+          <Card title={title} content={content} id={id} key={id} />
+        ))}
+      </GridTemplate>
+    );
+  }
+}
 
 Notes.propTypes = {
   notes: PropTypes.arrayOf(
     PropTypes.shape({
-      _id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      _id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       content: PropTypes.string.isRequired,
-      created: PropTypes.string.isRequired,
     }),
   ),
 };
@@ -26,6 +37,13 @@ Notes.defaultProps = {
   notes: [],
 };
 
-const mapStateToProps = ({ notes }) => ({ notes });
+const mapStateToProps = (state) => {
+  const { notes } = state;
+  return { notes };
+};
 
-export default connect(mapStateToProps)(Notes);
+const mapDispatchToProps = (dispatch) => ({
+  fetchNotes: () => dispatch(fetchItems('notes')),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Notes);
